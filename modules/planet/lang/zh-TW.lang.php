04fb68a9574dc3a97098eb2a092d27658fad5a45
royallin <royallin@ddbbfd98-3639-67cc-3805-8e79e4f39616>
2010-04-03 12:09:41 +0000

    Update Taiwanese

--- modules/planet/lang/zh-TW.lang.php
+++ modules/planet/lang/zh-TW.lang.php
@@ -2,48 +2,48 @@
     /**
      * @file   modules/planet/lang/zh-TW.lang.php
      * @author sol (sol@ngleader.com) 翻譯:royallin
-     * @brief  微型部落格(planet)模組正體中文語言
+     * @brief  微網誌(planet)模組正體中文語言
      **/
 
-    $lang->planet = "微型部落格";
+    $lang->planet = "微網誌";
     $lang->planet_title = "部落格標題";
     $lang->planet_url = "部落格網址";
-    $lang->planet_myplanet = "我的微型部落格";
-    $lang->planet_whos_planet = "%s 的微型部落格";
+    $lang->planet_myplanet = "我的微網誌";
+    $lang->planet_whos_planet = "%s 的微網誌";
     $lang->planet_whos_favorite = "%s 的最愛";
-    $lang->planet_whos_favorite_list = "'%s'的微型部落格清單";
+    $lang->planet_whos_favorite_list = "'%s'的微網誌清單";
     $lang->planet_favorite = "的最愛";
 
     $lang->planet_welcome = "歡迎光臨！";
 
-    $lang->planet_reply_content = "微型部落格的回覆內容";
+    $lang->planet_reply_content = "微網誌的回覆內容";
 
     $lang->about_planet =
-        "XE微型部落格模組，可讓會員建立自己的微型部落格。
-         虛擬網站模組所使用的網域名稱無法用於微型部落格模組。
-         如果想將微型部落格當作首頁，請先在網站模組中設定其他的網域名稱。";
+        "XE微網誌模組，可讓會員建立自己的微網誌。
+         虛擬網站模組所使用的網域名稱無法用於微網誌模組。
+         如果想將微網誌當作首頁，請先在網站模組中設定其他的網域名稱。";
 
-    $lang->planet_mid = "微型部落格連結網址";
-    $lang->about_planet_mid = "可以直接輸入網址訪問部落格，微型部落格連結網址是「http://主程式網址/微型部落格網址」。";
+    $lang->planet_mid = "微網誌連結網址";
+    $lang->about_planet_mid = "可以直接輸入網址訪問，微網誌連結網址是「http://主程式網址/微網誌網址」。";
 
-    $lang->planet_default_skin = "微型部落格面板";
-    $lang->about_planet_default_skin = "選擇微型部落格面板。";
+    $lang->planet_default_skin = "微網誌面板";
+    $lang->about_planet_default_skin = "選擇微網誌面板。";
 
-    $lang->planet_comment = "微型部落格簡介";
-    $lang->about_planet_comment = "微型部落格的簡介，同時也會顯示在瀏覽器的標題。";
+    $lang->planet_comment = "微網誌簡介";
+    $lang->about_planet_comment = "微網誌的簡介，同時也會顯示在瀏覽器的標題。";
 
     $lang->use_signup = "顯示會員註冊按鈕";
-    $lang->about_use_signup = "選擇此項，會在微型部落格首頁顯示『新會員註冊』按鈕。";
+    $lang->about_use_signup = "選擇此項，會在微網誌首頁顯示『會員註冊』按鈕。";
 
-    $lang->cmd_create_planet = "建立我的微型部落格";
-    $lang->create_message = "微型部落格簡介";
-    $lang->about_create_message = "簡單地輸入關於部落格的簡介。";
+    $lang->cmd_create_planet = "建立我的微網誌";
+    $lang->create_message = "微網誌簡介";
+    $lang->about_create_message = "簡單地輸入簡介。";
 
     $lang->cmd_planet_setup = "基本設定";
-    $lang->cmd_planet_list = "微型部落格列表";
+    $lang->cmd_planet_list = "微網誌列表";
 
     $lang->msg_not_logged = "請先登入。";
-    $lang->msg_planet_exists = "已建立微型部落格，無法再新增。";
+    $lang->msg_planet_exists = "已建立微網誌，無法再新增。";
     $lang->msg_denied_planet_title = 'Denied planet title';
 
     $lang->planet_userinfo = "個人資料";
@@ -57,7 +57,7 @@
     $lang->cmd_planet_addfavorite = "新增至最愛";
 
     $lang->planet_hot_tag = "熱門標籤";
-    $lang->planet_home = "微型部落格首頁";
+    $lang->planet_home = "微網誌首頁";
     $lang->cmd_planet_more_tag = "更多";
 
     $lang->planet_memo = "留言";
@@ -81,8 +81,8 @@
     $lang->cmd_planet_openclose_recommend_search = "開啟或關閉推薦關鍵字";
     $lang->about_planet_input_search_text = "輸入關鍵字";
 
-    $lang->about_planet_make_planet = "建立微型部落格。請輸入微型部落格的基本資料。";
-    $lang->about_planet_make_planet_info = "顯示微型部落格的基本資料。當所有項目都輸入完畢後，請儲存您的設定。";
+    $lang->about_planet_make_planet = "建立微網誌。請輸入微網誌的基本資料。";
+    $lang->about_planet_make_planet_info = "顯示微網誌的基本資料。當所有項目都輸入完畢後，請儲存您的設定。";
     $lang->planet_input_personalinfo = "輸入個人資料";
     $lang->planet_photo = "照片";
     $lang->planet_myintro = "自我介紹";
@@ -93,8 +93,8 @@
 
     $lang->about_planet_tag = "以「,」區分多個標籤。";
 
-    $lang->cmd_planet_makeOk_move_myplanet = "確認 : 移動到我的微型部落格。";
-    $lang->cmd_planet_ok_move_myplanet = "確認 : 移動到我的微型部落格。";
+    $lang->cmd_planet_makeOk_move_myplanet = "確認 : 移動到我的微網誌。";
+    $lang->cmd_planet_ok_move_myplanet = "確認 : 移動到我的微網誌。";
 
 
     $lang->about_planet_login = "請輸入帳號密碼。";
@@ -105,7 +105,7 @@
     $lang->planet_nowhot_tag = "熱門標籤";
     $lang->cmd_planet_close_nowhot_tag = "關閉熱門標籤";
 
-    $lang->about_planet_whats_textSearch_in_planet = "在<strong>'%s'</strong>的微型部落格中搜尋<strong>'%s'</strong>的結果。";
+    $lang->about_planet_whats_textSearch_in_planet = "在<strong>'%s'</strong>的微網誌中搜尋<strong>'%s'</strong>的結果。";
     $lang->about_planet_whats_textSearch = "所有關於<strong>'%s'</strong>的搜尋結果。";
 
     $lang->planet_acticle = "文章";
@@ -121,13 +121,13 @@
     $lang->planet_article_preview = "預覽";
 
 
-    $lang->planet_notice_title = "%s，歡迎來到微型部落格！<br>%s，您可以在微型部落格中分享您的想法、意見、資訊以及其他知識喔。以下是簡單的使用說明。^^";
+    $lang->planet_notice_title = "%s，歡迎來到微網誌！<br>%s，您可以在微網誌中分享您的想法、意見、資訊以及其他知識喔。以下是簡單的使用說明。^^";
     $lang->planet_notice_list = array(
         "底下的內容輸入區域可以開啟或關閉。",
-        "所有瀏覽者都能夠觀看和回覆%s的微型部落格。",
+        "所有瀏覽者都能夠觀看和回覆 %s 的微網誌。",
         "利用「新增最愛」與「喜愛標籤」功能，隨時都能看到%s的喜愛內容。",
-        "%s的「照片、暱稱、標籤」等資料都可在此頁面中進行修改。",
-        "對其他微型部落格有興趣嗎？請利用『熱門標籤』或『搜尋』。",
+        "%s 的「照片、暱稱、標籤」等資料都可在此頁面中進行修改。",
+        "對其他微網誌有興趣嗎？請利用『熱門標籤』或『搜尋』。",
         "如果您還有其他問題。請搜尋『問題』標籤。",
     );
     $lang->planet_notice_disable = "不要再顯示。";
@@ -137,16 +137,16 @@
     $lang->msg_planet_already_added_favorite = "已新增的最愛。";
     $lang->msg_planet_no_memo = "目前沒有留言";
 
-    $lang->msg_planet_rss_enabled = "RSS使用中";
-    $lang->msg_planet_rss_disabled = "暫停使用RSS";
+    $lang->msg_planet_rss_enabled = "RSS 使用中";
+    $lang->msg_planet_rss_disabled = "暫停使用 RSS";
 
-    $lang->msg_me2day_sync = "傳送到me2day";
-    $lang->msg_me2day_sync_q = "確定要傳送到me2day嗎？";
+    $lang->msg_me2day_sync = "傳送到 me2day";
+    $lang->msg_me2day_sync_q = "確定要傳送到 me2day 嗎？";
     $lang->me2day_id = "me2day網址";
     $lang->me2day_ukey = "User Key";
     $lang->msg_me2day_activate = "自動傳送。";
-    $lang->msg_fail_auth_me2day = "Me2day認證失敗。";
-    $lang->msg_success_auth_me2day = "Me2day認證成功。";
+    $lang->msg_fail_auth_me2day = "Me2day 認證失敗。";
+    $lang->msg_success_auth_me2day = "Me2day 認證成功。";
 
     $lang->planet_total_articles = "全部";
     $lang->planet_wantyou = "推薦";
@@ -167,12 +167,12 @@
     $lang->planet_tagtab_after = "新增標籤頁";
     $lang->about_planet_tagtab_after = "可新增多個標籤頁。以「,」區分多個標籤。";
     $lang->planet_smstag = "SMS標籤";
-    $lang->about_planet_smstag = "以「,」區分多個標籤，用SMS傳送時將自動新增標籤。";
+    $lang->about_planet_smstag = "以「,」區分多個標籤，用 SMS 傳送時將自動新增標籤。";
 
-    $lang->planet_use_mobile = "開啟SMS";
-    $lang->about_use_mobile = "可用手機發送SMS";
+    $lang->planet_use_mobile = "開啟 SMS";
+    $lang->about_use_mobile = "可用手機發送 SMS";
     $lang->planet_use_me2day = "Me2day";
-    $lang->about_use_me2day = "發表主題的同時也傳送到Me2day(http://me2day.net)。";
-    $lang->msg_search_thisplanet = "搜尋微型部落格";
-    $lang->msg_welcome_planet = '歡迎加入微型部落格^_^';
+    $lang->about_use_me2day = "發表主題的同時也傳送到 Me2day(http://me2day.net)。";
+    $lang->msg_search_thisplanet = "搜尋微網誌";
+    $lang->msg_welcome_planet = '歡迎加入微網誌^_^';
 ?>
