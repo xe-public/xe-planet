f333f9f1d087ad10d67a499f28c69bbb295ad522
admin@folderfile.net <admin@folderfile.net>
2012-08-10 12:17:40 +0000

    tag 0.1.1

---
+++ planet_bookmark/0.1.1/planet_bookmark.addon.php
@@ -0,0 +1,18 @@
+<?php
+    if(!defined("__ZBXE__")) exit();
+
+    /**
+     * @file planet_todo.addon.php
+     * @author SOL군 (sol@ngleader.com)
+     * @brief
+     **/
+    if($called_position == 'after_module_proc' && Context::getResponseMethod()!="XMLRPC" && Context::getResponseMethod()!="JSON") {
+        $planet = Context::get('planet');
+        if(!$planet || !$planet->isMyPlanet()) return;
+        $oPlanetModel = &getModel('planet');
+        $countBookmark = $oPlanetModel->getTagSearchResultCount($planet->getModuleSrl(),'bookmark');
+        Context::addHtmlHeader("<script type=\"text/javascript\">//<![CDATA[\nvar planet_bookmark_count=".( (int)$countBookmark).";\n//]]></script>");
+		Context::addHtmlHeader("<link rel=\"stylesheet\" href=\"./addons/planet_bookmark/style.css\" type=\"text/css\" />");
+        Context::addJsFile('./addons/planet_bookmark/planet_bookmark.js');
+    }
+?>
