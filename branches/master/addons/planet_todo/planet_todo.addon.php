8b212d1feada2c2260e9b89cbf1eaa0da1bced63
misol.kr <misol.kr@ddbbfd98-3639-67cc-3805-8e79e4f39616>
2014-08-25 05:06:28 +0000

    check out for branch/master

---
+++ branches/master/addons/planet_todo/planet_todo.addon.php
@@ -0,0 +1,23 @@
+<?php
+    if(!defined("__ZBXE__")) exit();
+
+    /**
+     * @file planet_todo.addon.php
+     * @author SOL군 (sol@ngleader.com)
+     * @brief
+     **/
+    if($called_position == 'after_module_proc' && Context::getResponseMethod()!="XMLRPC" && Context::getResponseMethod()!="JSON") {
+        $planet = Context::get('planet');
+        if(!$planet || !$planet->isMyPlanet()) return;
+
+        $oPlanetModel = &getModel('planet');
+        $countTodo = $oPlanetModel->getTagSearchResultCount($planet->getModuleSrl(),'todo');
+        $countDone = $oPlanetModel->getTagSearchResultCount($planet->getModuleSrl(),'done');
+
+		
+        Context::addHtmlHeader("<script type=\"text/javascript\">//<![CDATA[\nvar planet_todo_count={todo:".$countTodo.",done:".$countDone."};\n//]]></script>");
+		Context::addHtmlHeader("<link rel=\"stylesheet\" href=\"./addons/planet_todo/style.css\" type=\"text/css\" />");
+        Context::addJsFile('./addons/planet_todo/planet_todo.js');
+
+    }
+?>
